e7f0d35d1b99d5aa2b540ba3550952a2e6af10f8
JC-Coder <josephchimezie2003@gmail.com>
2024-07-03 09:06:09 +0100

    update

--- VisitorInfoApi/Services/VisitorInfoService.cs
+++ VisitorInfoApi/Services/VisitorInfoService.cs
@@ -31,11 +31,22 @@ namespace VisitorInfoApi.Services
 
         private async Task<IpStackResponse> GetIpStackInfo(string ipAddress)
         {
-            var baseUrl = "http://api.ipstack.com/";
-            var apiKey = "e7e3eea930007034729837c9774b231a";
+            // var baseUrl = "http://api.ipstack.com/";
+            // var apiKey = "e7e3eea930007034729837c9774b231a";
 
-            var url = $"{baseUrl}{ipAddress}?access_key={apiKey}";
-            
+            // var url = $"{baseUrl}{ipAddress}?access_key={apiKey}";
+
+            // var response = await _httpClient.GetFromJsonAsync<IpStackResponse>(url);
+
+            // if (response == null || string.IsNullOrEmpty(response.City))
+            // {
+            //     throw new Exception("Unable to determine location from IP address");
+            // }
+
+            // return response;
+
+            var url = $"https://api.ipgeolocation.io/ipgeo?apiKey=6e3e279ba70044d0ad992ea1ad1fe928&ip={ipAddress}&fields=city";
+
             var response = await _httpClient.GetFromJsonAsync<IpStackResponse>(url);
 
             if (response == null || string.IsNullOrEmpty(response.City))
@@ -53,7 +64,7 @@ namespace VisitorInfoApi.Services
             var apiKey = "HtTW81xFvAVfr1ykZY5zoGRbZXVejLUL";
 
             var url = $"{baseUrl}?location={latitude},{longitude}&apikey={apiKey}";
-            
+
             return await _httpClient.GetFromJsonAsync<TomorrowioResponse>(url);
         }
     }
